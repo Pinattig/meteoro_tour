9e25e315f45fd48d9fd68b2e3ba90fcada40d6df
Gustavo <gmpinatti@gmail.com>
2021-01-07 07:43:02 -0300

    Create GerenciarOnibus use case

---
+++ src/main/java/br/edu/ifsp/domain/usecases/onibus/GerenciarOnibusUseCase.java
@@ -0,0 +1,34 @@
+package br.edu.ifsp.domain.usecases.onibus;
+
+import br.edu.ifsp.domain.entities.onibus.Onibus;
+import br.edu.ifsp.utils.DAO;
+
+import java.util.List;
+
+public class GerenciarOnibusUseCase {
+    DAO onibusDAO;
+
+    public GerenciarOnibusUseCase(DAO onibusDAO) {
+        this.onibusDAO = onibusDAO;
+    }
+
+    public List<Onibus> getAll(){
+        return onibusDAO.findAll();
+    }
+
+    public Object create(Onibus onibus){
+        return onibusDAO.create(onibus);
+    }
+
+    public boolean update(Onibus onibus){
+        return onibusDAO.update(onibus);
+    }
+
+    public boolean delete(Onibus onibus){
+        return onibusDAO.delete(onibus);
+    }
+
+    public boolean deleteByKey(Object key){
+        return onibusDAO.deleteByKey(key);
+    }
+}
