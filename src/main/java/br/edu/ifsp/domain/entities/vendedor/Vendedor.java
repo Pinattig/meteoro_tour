c4da99c9a816148e0504bf0d030dc9612ab2455b
Gustavo <gmpinatti@gmail.com>
2021-01-07 19:50:56 -0300

    Add PropertieReader in to emitirPassagem

--- src/main/java/br/edu/ifsp/domain/entities/vendedor/Vendedor.java
+++ src/main/java/br/edu/ifsp/domain/entities/vendedor/Vendedor.java
@@ -2,7 +2,10 @@ package br.edu.ifsp.domain.entities.vendedor;
 
 import br.edu.ifsp.domain.entities.passagem.Passagem;
 import br.edu.ifsp.domain.entities.viagem.Viagem;
+import br.edu.ifsp.utils.PropertiesReader;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class Vendedor {
@@ -13,7 +16,22 @@ public class Vendedor {
     public Vendedor() {
     }
 
-    public void emitirPassagem(Passagem passagem) {
+    public void emitirPassagem(Passagem passagem) throws IOException {
+
+        PropertiesReader propertiesReader = new PropertiesReader("config.properties");
+        try{
+            String path = propertiesReader.getPropertieValue("SavePassagemFilePath");
+            String fileName = passagem.getNome()+"_"+passagem.getNumPassagem()+".txt";
+            PrintWriter writer = new PrintWriter(path+fileName, "UTF-8");
+
+            writer.println("-------- PASSAGEM RODOVIÁRIA --------");
+            writer.println("Nome do passageiro: " + passagem.getNome());
+            writer.println("CPF do passageiro: " + passagem.getCpf());
+
+            writer.close();
+        }catch (IOException e){
+            throw e;
+        }
 
     }
 
