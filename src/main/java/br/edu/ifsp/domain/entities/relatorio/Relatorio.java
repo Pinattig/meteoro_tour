472a3e0f17bf09463e616016d717443faca1fea4
Gustavo <gmpinatti@gmail.com>
2021-01-08 00:17:19 -0300

    Initialize SalvarEmArquivo method

--- src/main/java/br/edu/ifsp/domain/entities/relatorio/Relatorio.java
+++ src/main/java/br/edu/ifsp/domain/entities/relatorio/Relatorio.java
@@ -1,8 +1,14 @@
 package br.edu.ifsp.domain.entities.relatorio;
 
+import br.edu.ifsp.utils.PropertiesReader;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class Relatorio {
     private LocalDate dataInicio;
@@ -34,8 +40,21 @@ public class Relatorio {
         this.dataFinal = dataFinal;
     }
 
-    public void salvarEmArquivo() {
+    public void salvarEmArquivo() throws IOException {
+        PropertiesReader propertiesReader = new PropertiesReader("config.properties");
+        try{
+            String path = propertiesReader.getPropertieValue("SaveRelatorioFilePath");
+            UUID idRelatorio = UUID.randomUUID();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String fileName = "Relatorio_"+idRelatorio+"_"+LocalDate.now().format(formatter)+".csv";
+            PrintWriter writer = new PrintWriter(path+fileName, "UTF-8");
+            //writer.println("o;green;falou;ta;falado");
+            //Inserir logica para montar EXCEL aqui bjos
 
+            writer.close();
+        }catch (IOException e){
+            throw e;
+        }
     }
 
     @Override
