71c519296891e092d4ebafbf70629fd48c2448b7
Pedro Andrade <andrade.batista@aluno.ifsp.edu.br>
2021-01-09 19:08:25 -0300

    Create UseCase of "devolver Passagens"

---
+++ src/main/java/br/edu/ifsp/domain/usecases/passagem/DevolverPassagemUseCase.java
@@ -0,0 +1,31 @@
+package br.edu.ifsp.domain.usecases.passagem;
+
+import br.edu.ifsp.domain.entities.passagem.Passagem;
+import br.edu.ifsp.domain.usecases.viagem.GerarViagemUseCase;
+import br.edu.ifsp.utils.exceptions.NotAvaliableSeatException;
+import br.edu.ifsp.utils.exceptions.PassageNotFoundException;
+
+import java.util.Optional;
+
+public class DevolverPassagemUseCase {
+
+    private PassagemDAO passagemDAO;
+    private ConsultarPassagemVendidaUseCase consultarPassagemVendidaUseCase;
+
+
+    public DevolverPassagemUseCase(PassagemDAO passagemDAO, ConsultarPassagemVendidaUseCase consultarPassagemVendidaUseCase) {
+        this.passagemDAO = passagemDAO;
+        this.consultarPassagemVendidaUseCase = consultarPassagemVendidaUseCase;
+
+    }
+    public void devolverPassagem(Optional<Passagem> passagem) {
+        
+        if(consultarPassagemVendidaUseCase.consultarPassagem(passagem.get().getNumPassagem(),passagem.get().getNome()).isEmpty())
+            throw new PassageNotFoundException("A passagem não foi encontrada");
+
+        if(!passagem.get().verificarValidade())
+            throw new NotAvaliableSeatException("Não há assentos disponiveis");
+
+        passagemDAO.deleteByKey(passagem.get().getNumPassagem());
+    }
+}
