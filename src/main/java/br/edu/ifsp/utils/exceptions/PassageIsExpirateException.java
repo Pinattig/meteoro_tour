c413887f551203c2b8873b6aa65d8555deec6583
Pedro Andrade <andrade.batista@aluno.ifsp.edu.br>
2021-01-09 19:06:39 -0300

    Create Exception of passage is expired

---
+++ src/main/java/br/edu/ifsp/utils/exceptions/PassageIsExpirateException.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.utils.exceptions;
+
+public class PassageIsExpirateException extends RuntimeException{
+    public PassageIsExpirateException(String message) {
+        super(message);
+    }
+}
